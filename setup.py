6b1619673165c7c8f4e087989b56fbb48dfe9b79
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2013-04-05 12:13:41 +0200

    Setup completed and working

--- setup.py
+++ setup.py
@@ -4,47 +4,55 @@ Created on 25/02/2013
 @author: victor
 '''
 from distutils.core import setup, Extension
-import numpy
-setup(name='pyRMSD',
-      version='3.0',
-      description='pyRMSD is a small Python package that aims to offer an integrative and efficient way of performing RMSD calculations of large sets of structures. It is specially tuned to do fast collective RMSD calculations, as pairwise RMSD matrices.',
+
+setup(name='pyProClust',
+      version='1.0',
+      description='',
       author='Victor Alejandro Gil Sepulveda',
       author_email='victor.gil.sepulveda@gmail.com',
-      url='https://github.com/victor-gil-sepulveda/pyRMSD.git',
-      packages=['pyRMSD','pyRMSD.utils'],
-      package_dir={'pyRMSD':'./pyRMSD'},
-      py_modules=['pyRMSD.availableCalculators', 'pyRMSD.matrixHandler', 'pyRMSD.RMSDCalculator', 'pyRMSD.utils.proteinReading'],
-      include_dirs = [numpy.get_include()],
+      url='https://github.com/victor-gil-sepulveda/pyProClust',
+      packages=[
+                'pyproclust',
+                'pyproclust.algorithms',
+                'pyproclust.algorithms.hierarchical',
+                'pyproclust.algorithms.spectral',
+                'pyproclust.algorithms.dbscan',
+                'pyproclust.algorithms.kmedoids',
+                'pyproclust.algorithms.random',
+                'pyproclust.algorithms.gromos',
+                'pyproclust.htmlreport',
+                'pyproclust.tools',
+                'pyproclust.clustering',
+                'pyproclust.clustering.analysis',
+                'pyproclust.clustering.comparison',
+                'pyproclust.clustering.comparison.distrprob',
+                'pyproclust.clustering.selection',
+                'pyproclust.clustering.metrics',
+                'pyproclust.clustering.metrics.cython',
+                'pyproclust.clustering.filtering',
+                'pyproclust.protocol',
+                'pyproclust.protocol.refinement',
+                'pyproclust.protocol.exploration',
+                'pyproclust.driver',
+                'pyproclust.driver.handlers',
+                'pyproclust.driver.handlers.matrix',
+                'pyproclust.driver.observer',
+                'pyproclust.driver.scheduling',
+                'pyproclust.driver.compressor'
+      ],
       ext_modules=[
-                   Extension('pyRMSD.pdbReader',[
-                                          'src/pdbreaderlite/PDBReader.cpp',
-                                          'src/python/readerLite.cpp'
+                   Extension('pyproclust.clustering.metrics.cython.normNCut',[
+                                'pyproclust/clustering/metrics/cython/normNCut.c'                         
                    ]),
-                   Extension('pyRMSD.condensedMatrix', [
-                                                 'src/matrix/Matrix.cpp',
-                                                 'src/matrix/Statistics.cpp'
+                   Extension('pyproclust.clustering.metrics.cython.boundedCohesion', [
+                                'pyproclust/clustering/metrics/cython/boundedCohesion.c'                                                 
                    ]),
-                   Extension(
-                             'pyRMSD.calculators',
-                             sources = [
-                                        'src/python/pyRMSD.cpp',
-                                        
-                                        'src/calculators/RMSDCalculator.cpp',
-                                        'src/calculators/RMSDTools.cpp',
-                                        'src/calculators/factory/RMSDCalculatorFactory.cpp',
-
-                                        'src/calculators/KABSCH/KABSCHSerialKernel.cpp',
-                                        'src/calculators/KABSCH/KABSCHOmpKernel.cpp',
-                                        
-                                        'src/calculators/QTRFIT/QTRFITSerialKernel.cpp',
-                                        'src/calculators/QTRFIT/QTRFITOmpKernel.cpp',
-                                        
-                                        'src/calculators/QCP/QCPSerialKernel.cpp',
-                                        'src/calculators/QCP/QCPOmpKernel.cpp',
-                             ],
-                             extra_compile_args=['-fopenmp'],
-                             extra_link_args=['-fopenmp']
-                   )
+                   Extension('pyproclust.clustering.metrics.cython.silhouette',[
+                                'pyproclust/clustering/metrics/cython/silhouette.c'                                       
+                   ]),
+                   Extension('pyproclust.clustering.metrics.cython.meanMinimumDistance', [
+                                'pyproclust/clustering/metrics/cython/meanMinimumDistance.c'                                                        
+                   ])
       ],
       
      )
