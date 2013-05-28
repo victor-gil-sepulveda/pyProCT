aff3597b1aaacad3d580a5b1e89c4a3feae8e90f
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2013-05-28 19:38:56 +0200

    added failing test (TODO test)

---
+++ pyproclust/driver/handlers/test/TestTrajectoryHandler.py
@@ -0,0 +1,18 @@
+'''
+Created on 28/03/2013
+
+@author: victor
+'''
+import unittest
+
+
+class TestTrajectoryHandler(unittest.TestCase):
+
+
+    def test_get_joined_pdbs(self):
+        self.fail("TODO")
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_get_joined_pdbs']
+    unittest.main()
