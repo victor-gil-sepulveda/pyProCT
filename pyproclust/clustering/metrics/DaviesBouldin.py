23587a0cd740f2baeb45fd7627c69f99c7e06d78
vgil <victor.gil.sepulveda@gmail.com>
2013-05-30 19:02:01 +0200

    DaviesBoulding metric CVI added (skeleton)

---
+++ pyproclust/clustering/metrics/DaviesBouldin.py
@@ -0,0 +1,17 @@
+'''
+Created on May 30, 2013
+
+@author: victor
+'''
+
+class DaviesBouldinCalculator(object):
+    def __init__(self):
+        pass
+    
+    def evaluate(self, clustering, matrix):
+        pass
+    
+    def evaluate_cluster(self, cluster, condensed_distance_matrix):
+        """
+        """
+        pass
