60df9c7481b657995156512507281b1f0a36f006
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2013-06-11 18:00:07 +0200

    Test started

--- pyproclust/clustering/metrics/test/TestCalinskiHarabasz.py
+++ pyproclust/clustering/metrics/test/TestCalinskiHarabasz.py
@@ -4,13 +4,28 @@ Created on 11/06/2013
 @author: victor
 '''
 import unittest
+from pyproclust.clustering.metrics.test.data import CH_table1
+from pyproclust.clustering.metrics.CalinskiHarabasz import CalinskiHarabaszCalculator
+from pyproclust.clustering.clustering import Clustering
+from pyproclust.clustering.cluster import Cluster
+from pyRMSD.condensedMatrix import CondensedMatrix
 
 
 class Test(unittest.TestCase):
 
 
-    def testName(self):
-        pass
+    def test_evaluation(self):
+        clusterings = [
+                       {
+                        "clustering": Clustering([Cluster(None, [0,1,2,3]),Cluster(None, [5,6])]),
+                        "result": 3.74
+                        },
+                       ]
+        calculator = CalinskiHarabaszCalculator()
+        matrix = CondensedMatrix(CH_table1)
+        
+        for i in range(len(clusterings)):        
+            self.assertEqual(clusterings[i]["result"], calculator.evaluate(clusterings[i]["clustering"], matrix ))
 
 
 if __name__ == "__main__":
