9518cb5f2b2aaa2de71ea394fa9b9d6422cf91b2
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2014-05-15 10:26:57 +0200

    Skeleton created

---
+++ pyproct/algorithms/mcl/mclAlgorithm.py
@@ -0,0 +1,16 @@
+'''
+Created on 15/05/2014
+
+@author: victor
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
---
+++ pyproct/algorithms/mcl/test/TestMclAlgorithm.py
@@ -0,0 +1,18 @@
+'''
+Created on 15/05/2014
+
+@author: victor
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
