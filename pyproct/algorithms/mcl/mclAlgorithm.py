ecb2e3d0254d22e10f0b16afb54bab15feb46df2
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2014-05-19 12:38:00 +0200

    skeleton added

--- pyproct/algorithms/mcl/mclAlgorithm.py
+++ pyproct/algorithms/mcl/mclAlgorithm.py
@@ -8,9 +8,19 @@ class MyClass(object):
     '''
     classdocs
     '''
+    # Any power of a symmetric matrix is a symmetric matrix
+    # G is a complete graph where arcs are weighted by the distance of the elements represented by vertices
+    # weight function  (distance or similarity) must be symmetric
+    # M is the matrix containing the weights
 
+    def __init__(self, condensed_matrix, **kwargs):
+        pass
 
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
+    def perform_clustering(self, kwargs):
+        pass
+
+    def expansion(self):
+        pass
+
+    def inflation(self):
+        pass
