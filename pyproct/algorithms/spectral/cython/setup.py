404cb89897810ee1b7686c53c15676337011d9b2
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2014-06-02 10:38:38 +0200

    setup files protected to work with sphynx

--- pyproct/algorithms/spectral/cython/setup.py
+++ pyproct/algorithms/spectral/cython/setup.py
@@ -6,8 +6,9 @@ Created on 26/07/2012
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-import numpy as np  
-setup(
-    cmdclass = {'build_ext': build_ext},
-    include_dirs = [np.get_include()], 
-    ext_modules = [Extension("spectralTools", ["spectralTools.pyx"],extra_compile_args=["-O3","-ffast-math"])])
+import numpy as np
+if __name__ == '__main__': # Comp. with sphynx
+    setup(
+        cmdclass = {'build_ext': build_ext},
+        include_dirs = [np.get_include()],
+        ext_modules = [Extension("spectralTools", ["spectralTools.pyx"],extra_compile_args=["-O3","-ffast-math"])])
--- pyproct/clustering/metrics/cython/setup.py
+++ pyproct/clustering/metrics/cython/setup.py
@@ -6,12 +6,14 @@ Created on 26/07/2012
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-import numpy as np  
-setup(
-    cmdclass = {'build_ext': build_ext},
-    include_dirs = [np.get_include()], 
-    ext_modules = [Extension("boundedCohesion", ["boundedCohesion.pyx"],extra_compile_args=["-O3","-ffast-math"]),
-    Extension("silhouette", ["silhouette.pyx"],extra_compile_args=["-O3","-ffast-math"]),
-    Extension("meanMinimumDistance", ["meanMinimumDistance.pyx"],extra_compile_args=["-O3","-ffast-math"]),
-    Extension("normNCut", ["normNCut.pyx"],extra_compile_args=["-O3","-ffast-math"])
-    ])
+import numpy as np
+
+if __name__ == '__main__': # Comp. with sphynx
+    setup(
+        cmdclass = {'build_ext': build_ext},
+        include_dirs = [np.get_include()],
+        ext_modules = [Extension("boundedCohesion", ["boundedCohesion.pyx"],extra_compile_args=["-O3","-ffast-math"]),
+        Extension("silhouette", ["silhouette.pyx"],extra_compile_args=["-O3","-ffast-math"]),
+        Extension("meanMinimumDistance", ["meanMinimumDistance.pyx"],extra_compile_args=["-O3","-ffast-math"]),
+        Extension("normNCut", ["normNCut.pyx"],extra_compile_args=["-O3","-ffast-math"])
+        ])
