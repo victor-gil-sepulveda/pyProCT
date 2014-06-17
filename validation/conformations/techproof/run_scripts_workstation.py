58a944b6f2d2b6802704f5eb2cb3752d36585ef4
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2014-06-17 09:17:33 +0200

    backup of script used to run compressions on workstation

---
+++ validation/conformations/techproof/run_scripts_workstation.py
@@ -0,0 +1,28 @@
+'''
+Created on 05/06/2014
+
+@author: victor
+'''
+
+import sys
+import os.path
+import pyproct.tools.scriptTools as tools
+
+level = sys.argv[1]
+
+execution_range = int(sys.argv[2]) , int(sys.argv[3])+1 #second is included
+
+exec_path = os.getcwd()
+
+base_folder = {
+              "level2":"scripts",
+              "level1":"",
+              "level0":""
+              }
+
+base = os.path.join(base_folder[level], level, "run")
+tools.create_directory(base)
+
+for script_number in range(*execution_range):
+    pyproct_script = os.path.join(base_folder[level], level, "script_%s_%d.json"%(level,script_number))
+    os.system("python -m pyproct.main %s"%pyproct_script)
