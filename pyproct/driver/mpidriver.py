6bd1583d2ef378ded4cdcf98b92440ef910e7cac
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2014-07-16 10:53:38 +0200

    MPI driver updated.

--- pyproct/driver/mpidriver.py
+++ pyproct/driver/mpidriver.py
@@ -7,89 +7,73 @@ from pyproct.driver.handlers.timerHandler import TimerHandler
 from pyproct.driver.handlers.trajectoryHandler import TrajectoryHandler
 from pyproct.driver.driver import Driver
 from mpi4py import MPI
-import copy_reg
 from pyRMSD.condensedMatrix import CondensedMatrix
-import pyRMSD.condensedMatrix
 from pyproct.driver.handlers.matrix.matrixHandler import MatrixHandler
+from pyproct.tools.commonTools import timed_method
+from pyproct.driver.handlers.MPIWorkspaceHandler import MPIWorkspaceHandler
 
 class MPIDriver(Driver):
 
     def __init__(self, observer):
         super(MPIDriver, self).__init__(observer)
         self.comm = MPI.COMM_WORLD
+        self.nprocs = self.comm.Get_size()
         self.rank = self.comm.Get_rank()
         self.workspaceHandler = None
         self.trajectoryHandler = None
         self.matrixHandler = None
 
-    def perform_actions(self, parameters):
-        best_clustering, clustering_results = self.get_best_clustering(parameters)
-        self.comm.Barrier()
-        if self.rank == 0:
-            self.postprocess(parameters, best_clustering)
-            #################################
-            # Results are saved to a file
-            #################################
-            self.save_clustering_results(clustering_results)
-
-            #################################
-            # Small summary of the best_cluster is shown
-            #################################
-            if parameters["clustering"]["generation"]["method"] != "load":
-                print "======================="
-                print "This is the chosen cluster:"
-                print "\t- Used algorithm: ", best_clustering['type']
-                print "\t- Number of clusters: ", best_clustering['evaluation']['Number of clusters']
-                print "\t- Noise: %.3f %%"%best_clustering['evaluation']['Noise level']
-
+    @timed_method(Driver.timer, "Global")
     def run(self, parameters):
-        #####################
-        # Start timing
-        #####################
-        self.timer = TimerHandler()
-        self.timer.start("Global")
 
-        #####################
-        # Workspace Creation
-        #####################
+        with MPIWorkspaceHandler(self.rank, 0, parameters["global"]["workspace"], self.observer) as self.workspaceHandler:
+            self.comm.Barrier()
+
+            if self.rank == 0:
+                self.save_parameters_file(parameters)
+
+            if "data" in parameters:
+                self.data_section(parameters)
+
+                if "clustering" in parameters:
+                    clustering_results = self.clustering_section(parameters)
+                    self.comm.Barrier()
+
+                    if self.rank == 0:
+                        self.postprocess(parameters, clustering_results)
+                        self.save_results(clustering_results)
+                        self.show_summary(parameters, clustering_results)
+
         if self.rank == 0:
-            self.create_workspace(parameters)
+            self.notify("MPI-Driver Finished", "\n"+str(self.timer))
+
+    def data_section(self, parameters):
+        matrix_parameters = parameters["data"]["matrix"]
+
+        self.load_trajectory(parameters)
         self.comm.Barrier()
-        self.workspaceHandler = self.comm.bcast(self.workspaceHandler, root=0)
 
-        #####################
-        # Saving Parameters
-        #####################
         if self.rank == 0:
-            self.save_parameters_file(parameters)
-
-        #####################
-        # Trajectory Loading
-        #####################
-        self.timer.start("Trajectory Loading")
-        self.trajectoryHandler = TrajectoryHandler(parameters, self.observer)
-        self.comm.Barrier()
-        self.timer.stop("Trajectory Loading")
-
-        ##############################
-        # Distance Matrix Generation
-        ##############################
-        if self.rank == 0:
-            self.create_matrix(parameters)
+            self.calculate_matrix(parameters)
         else:
-            self.matrixHandler = MatrixHandler(parameters["data"]["matrix"])
-            self.matrixHandler.distance_matrix = CondensedMatrix([0.])
+            self.matrixHandler = MatrixHandler(matrix_parameters)
+            self.matrixHandler.distance_matrix = CondensedMatrix([0.]) # fake matrix
         self.comm.Barrier()
+
         matrix_contents = list(self.comm.bcast(self.matrixHandler.distance_matrix.get_data(), root=0))
         if self.rank != 0:
             self.matrixHandler.distance_matrix = CondensedMatrix(matrix_contents)
-
-        ##############################
-        # Actions
-        ##############################
         self.comm.Barrier()
-        self.perform_actions(parameters)
 
-        self.timer.stop("Global")
-        self.notify("MPI-Driver Finished", "\n"+str(self.timer))
+        if self.nprocs > 1:
+
+            if self.rank == 0:
+                if "filename" in matrix_parameters:
+                    self.save_matrix(parameters)
+
+            if self.rank == 1:
+                if "image" in matrix_parameters:
+                    self.plot_matrix(parameters)
+
+
 
