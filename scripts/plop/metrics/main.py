4965a2fe300390bb4e10c5fc29f1123348f30300
victor-gil-sepulveda <victor.gil.sepulveda@gmail.com>
2014-08-06 14:59:05 +0200

    Old scripts moved to their own repos.

--- scripts/plop/metrics/main.py
+++
@@ -1,6 +0,0 @@
-from metrics import processDir, genMetricsFile
-import sys
-
-records = processDir(sys.argv[1],"traj")
-print records
-genMetricsFile("metrics.dat", ["Energy","L1BindingEne"], records)
